30fe8e9fd3292a4a1e8fa07a7b57925614d53be6
Souravsinha96 <souravsinha1604@gmail.com>
2022-04-03 21:33:24 +0530

    added google login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,44 @@
-import React, { useEffect } from "react";
+import { Button } from "@mui/material";
+import React, { useEffect, useRef } from "react";
 import classes from "./Login.module.scss";
+
 const Login = () => {
-  return <div id="buttonDiv">dsdsd</div>;
+  let auth = useRef("");
+  useEffect(() => {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId: process.env.REACT_APP_GOOLE_CLIENT_ID,
+          scope: "email",
+        })
+        .then(() => {
+          auth.current = window.gapi.auth2.getAuthInstance();
+          onAuthChange(auth.current.isSignedIn.get());
+          auth.current.isSignedIn.listen(onAuthChange);
+        });
+    });
+  }, []);
+  const onAuthChange = (isSignedIn) => {
+    console.log(auth.current);
+  };
+  const onSignInClick = () => {
+    auth.current.signIn();
+  };
+  const onSignOutClick = () => {
+    auth.current.signOut();
+  };
+  console.log(auth.current);
+  return (
+    <>
+      {" "}
+      <Button onClick={onSignInClick} variant="contained" color="primary">
+        Sign In
+      </Button>
+      <Button onClick={onSignOutClick} variant="contained" color="error">
+        Sign out
+      </Button>
+    </>
+  );
 };
 
 export default Login;
