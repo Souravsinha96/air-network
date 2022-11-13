e89327d52df8c3043dd21481c2d0a738a11a456e
Sourav Sinha <souravsinha1604@gmail.com>
2022-11-13 14:43:22 +0530

    small change

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,13 +1,13 @@
-import { loginApi } from "../api/apiEndpoints";
-import axios from "axios";
-import store from "../redux/Store";
-import { login } from "../redux/slices/userSlice";
-import { toast } from "react-toastify";
+import { loginApi } from '../api/apiEndpoints';
+import axios from 'axios';
+import store from '../redux/Store';
+import { login } from '../redux/slices/userSlice';
+import { toast } from 'react-toastify';
 
 let refresh = false;
 
-axios.defaults.headers.post["Content-Type"] =
-  "application/x-www-form-urlencoded";
+axios.defaults.headers.post['Content-Type'] =
+  'application/x-www-form-urlencoded';
 
 // axiosConfig.interceptors.request.use((config) => {
 //   const state = store.getState();
@@ -19,25 +19,25 @@ axios.defaults.headers.post["Content-Type"] =
 axios.interceptors.response.use(
   (resp) => resp,
   async (error) => {
-    toast.error(error.response?.data.errors[0].title);
+    toast.error(error.response?.data?.errors[0].title);
     if (error.response.status === 401 && !refresh) {
       refresh = true;
-      console.log("reuns");
+      console.log('reuns');
 
       const response = await axios.request({
         url: loginApi,
-        method: "POST",
+        method: 'POST',
         transformRequest: (data, headers) => {
-          delete headers.common["Authorization"];
+          delete headers.common['Authorization'];
           return data;
         },
         data: `grant_type=client_credentials&client_id=${process.env.REACT_APP_AMADEUS_API_KEY}&client_secret=${process.env.REACT_APP_AMADEUS_API_SECRET}`,
       });
-      console.log(response, response.data["access_token"]);
+      console.log(response, response.data['access_token']);
       if (response.status === 200) {
         axios.defaults.headers.common[
-          "Authorization"
-        ] = `Bearer ${response.data["access_token"]}`;
+          'Authorization'
+        ] = `Bearer ${response.data['access_token']}`;
         store.dispatch(login(response.data));
         return axios(error.config);
       }
